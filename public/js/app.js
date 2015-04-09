f3edce254c2b6eda948a503258515250557dfb20
Henry <hc49436@students.westport.k12.ct.us>
2015-04-09 11:20:33 -0400

    Fixed quadratic formula

--- public/js/app.js
+++ public/js/app.js
@@ -11,8 +11,8 @@ function quadratic() {
     var a = parseFloat($('#quada').val());
     var b = parseFloat($('#quadb').val());
     var c = parseFloat($('#quadc').val());
-    var x1 = -(-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
-    var x2 = -(-b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+    var x1 = (-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+    var x2 = (-b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
     var output1 = "The first x intercept is " + x1;
     var output2 = "The second x intercept is " + x2;
     $('#quadoutput').html([output1, output2].join(', '));
